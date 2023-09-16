bbdab6ed4eb668f0efc431fc796e511ecd695a44
Vishnu Paduchuri <paduchuri.v@northeastern.edu>
2023-09-15 20:21:24 -0400

    Lab 1 Assignment

---
+++ Lab_1/src/model/Product.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author vishnupaduchuri
+ */
+public class Product {
+    
+    private String name;
+    private String description;
+    private String availabilityCount;
+    private String price;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAvailabilityCount() {
+        return availabilityCount;
+    }
+
+    public void setAvailabilityCount(String availabilityCount) {
+        this.availabilityCount = availabilityCount;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+    
+    
+    
+}
