76a96ef72b62cc7761b93905fe66f36804853d21
Vishnu Paduchuri <paduchuri.v@northeastern.edu>
2023-11-12 21:09:07 -0500

    Lab 5 initial commit with models

---
+++ Lab_5/src/model/Business.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author vishnupaduchuri
+ */
+public class Business {
+
+    MasterOrderList masterOrderList;
+    SupplierDirectory supplierDirectory;
+
+    public Business() {
+        this.masterOrderList = new MasterOrderList();
+        this.supplierDirectory = new SupplierDirectory();
+    }
+
+    public MasterOrderList getMasterOrderList() {
+        return masterOrderList;
+    }
+
+    public void setMasterOrderList(MasterOrderList masterOrderList) {
+        this.masterOrderList = masterOrderList;
+    }
+
+    public SupplierDirectory getSupplierDirectory() {
+        return supplierDirectory;
+    }
+
+    public void setSupplierDirectory(SupplierDirectory supplierDirectory) {
+        this.supplierDirectory = supplierDirectory;
+    }
+}
